2607376eff50686ba02b3299fbc9e031c09ee1d6
Hasan Ali <hasan.ali@lacritz.com>
2021-10-04 21:06:22 +0530

    Added db connection

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
+require('colors');
 const express = require('express');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 const app = express();
+
+// Connect database
+connectDB();
 
 // routes
 const bootcamps = require('./routes/bootcamps');
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // entry route
 app.get('/', (req, res) => {
@@ -15,6 +26,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(`Server listening in ${process.env.NODE_ENV} mode on ${PORT}`)
+const server = app.listen(PORT, () =>
+  console.log(
+    `Server listening in ${process.env.NODE_ENV} mode on ${PORT} PORT`.yellow
+      .bold
+  )
 );
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error:${err.message}`.red);
+  server.close(() => process.exit(1));
+});
