94ec40a66cd450a1a247ec72db9de35a579a8baf
Hasan Ali <hasan.ali@lacritz.com>
2021-10-07 21:30:54 +0530

    Added slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -98,5 +99,10 @@ const BootcampSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+BootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
